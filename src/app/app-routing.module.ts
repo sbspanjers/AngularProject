84bfc4fa6ca6682de18ca1dc9586c9f23b9f1658
sbspanjers <sb.spanjers@gmail.com>
2022-11-16 13:43:40 +0100

    start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'user', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
